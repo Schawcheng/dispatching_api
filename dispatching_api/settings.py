362a3e61356bc46fdfaf8698219f13701cf2f27f
Schawcheng <jyanchou@163.com>
2024-06-18 16:11:20 +0800

    添加agent, bank, customer, recharge, withdraw app 到INSTALLED_APPS

--- dispatching_api/settings.py
+++ dispatching_api/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'card',
-    'backstage'
+    'backstage',
+    'agent',
+    'bank',
+    'customer',
+    'recharge',
+    'withdraw'
 ]
 
 MIDDLEWARE = [
